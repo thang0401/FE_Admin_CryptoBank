9b88734a35626f59ad7030af415c0b7c72eac8bb
Thanh <name123091@gmail.com>
2025-04-09 11:43:27 +0700

    fix bug of usdc-orders

--- src/pages/usdc-orders-management/index.tsx
+++ src/pages/usdc-orders-management/index.tsx
@@ -1,11 +1,19 @@
-// import React from 'react'
-// import OrdersManagementPage from 'src/components/orders/OrdersManagement'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+import OrdersManagementPage from 'src/components/orders/OrdersManagement'
 
 
-// const index = () => {
-//   return (
-//     <OrdersManagementPage/>
-//   )
-// }
+const Index = () => {
+    const router = useRouter()
 
-// export default index
+    useEffect(() => {
+      // Chuyển hướng đến trang đơn hàng mua theo mặc định
+      router.replace("/usdc-orders-management/buy-orders")
+    }, [router])
+  
+    // Trả về null vì trang này chỉ dùng để chuyển hướng
+    return null
+}
+
+export default Index
+
